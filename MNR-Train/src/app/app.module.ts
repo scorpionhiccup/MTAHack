849f63d2959e20ceebfb5f491467ee3ee73b62f2
Rajat R Hande <rajatrhande@Rajats-MBP.stonybrook.edu>
2020-07-15 11:38:12 -0400

    App contents updated. There was an issue with .gitignore previously

---
+++ MNR-Train/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StationListComponent } from './station-list/station-list.component';
+import { TrainDescriptionComponent } from './train-description/train-description.component';
+
+@NgModule({
+  declarations: [AppComponent, StationListComponent, TrainDescriptionComponent],
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ MNR-Train/src/app/tabs/tabs-routing.module.ts
@@ -0,0 +1,54 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TabsPage } from './tabs.page';
+import { StationListComponent } from '../station-list/station-list.component';
+import { TrainDescriptionComponent } from '../train-description/train-description.component';
+
+const routes: Routes = [
+  {
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {
+        path: 'stationInfo/:title',
+        component: StationListComponent
+      },
+      {
+        path: 'trainDesc/:source/:trainNumber',
+        component: TrainDescriptionComponent
+      },
+      {
+        path: 'tab1',
+        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+      },
+      {
+        path: 'tab2', redirectTo: 'stationInfo/stations'
+      },
+      {
+        path: 'tab3',
+        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+      },
+      // Lets redirect tab4 to tab2Module as tab2 is redirected to stations page
+      {
+        path: 'tab4',
+        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+      },
+      {
+        path: '',
+        redirectTo: '/tabs/tab1',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/tabs/tab1',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class TabsPageRoutingModule {}
